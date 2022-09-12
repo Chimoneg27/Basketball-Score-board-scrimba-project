6e0a2eff9a869fea79c161472a407c675ddd010d
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2022-09-12 15:14:39 +0200

    Basketball Score board

---
+++ script.js
@@ -0,0 +1,44 @@
+let scoreHome = 0;
+let scoreGuest = 0;
+
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+
+function addOne(){
+    scoreHome += 1;
+    homeScore.textContent = scoreHome;
+}
+
+function addOneGuest(){
+    scoreGuest += 1;
+    guestScore.textContent = scoreGuest;
+}
+
+function addTwo(){
+    scoreHome += 2;
+    homeScore.textContent = scoreHome;
+}
+
+function addTwoGuest(){
+    scoreGuest += 2;
+    guestScore.textContent = scoreGuest;
+}
+
+function addThree(){
+    scoreHome += 3;
+    homeScore.textContent = scoreHome;
+}
+
+function addThreeGuest(){
+    scoreGuest += 3;
+    guestScore.textContent = scoreGuest;
+}
+
+
+function reset(){
+    if (scoreGuest > 0 && scoreHome > 0){
+        return guestScore.textContent = 0, homeScore.textContent = 0;
+    }
+    scoreHome = 0;
+    scoreGuest = 0;
+}
